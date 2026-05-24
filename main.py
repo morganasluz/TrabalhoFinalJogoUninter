fca70f38a500e6a55aa90d6dc6cc0d4ff3d30c06
Morgana Luz <morganasluz@gmail.com>
2026-05-24 19:42:04 -0300

    organizei o main

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+print('Setup Start')
+window = pygame.display.set_mode(size=(600, 480))  # janela inicial
+print('Setup End')
+
+print('Loop Start')
+
+while True:
+    #check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #close window
+            quit() #end pygame
